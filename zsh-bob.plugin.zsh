7bc60a551709335aa9b5646ab78c951848b81244
Matthew Winter <33818+wintermi@users.noreply.github.com>
2024-01-13 21:16:17 +1100

    feat: modify the way the bob completions are managed

--- zsh-bob.plugin.zsh
+++ zsh-bob.plugin.zsh
@@ -14,11 +14,17 @@ path=("$HOME/.local/share/bob/nvim-bin" $path)
 # Completions directory for `bob` command
 local COMPLETIONS_DIR="${0:A:h}/completions"
 
-# Only regenerate completions if older than 7 days, or does not exist
-if [[ ! -f "$COMPLETIONS_DIR/_bob"  ||  ! $(find "$COMPLETIONS_DIR/_bob" -newermt "7 days ago" -print) ]]; then
-    bob complete zsh >| "$COMPLETIONS_DIR/_bob"
-fi
-
 # Add completions to the FPATH
 typeset -TUx FPATH fpath
 fpath=("$COMPLETIONS_DIR" $fpath)
+
+# If the completion file does not exist yet, then we need to autoload
+# and bind it to `bob`. Otherwise, compinit will have already done that.
+if [[ ! -f "$COMPLETIONS_DIR/_bob" ]]; then
+    typeset -g -A _comps
+    autoload -Uz _bob
+    _comps[bob]=_bob
+fi
+
+# Generate and load completion in the background
+bob complete zsh >| "$COMPLETIONS_DIR/_bob" &|
